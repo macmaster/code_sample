3a94e3fa461b0861dc9760c452ac9bdba13d3a2b
ronny macmaster <ronnymacmaster@gmail.com>
2015-12-27 00:17:10 -1000

    Reorg & final proj

---
+++ Java/UnfoldingMaps/src/final_src/AirportMarker.java
@@ -0,0 +1,55 @@
+package module6;
+
+import java.util.List;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** 
+ * A class to represent AirportMarkers on a world map.
+ *   
+ * @author Adam Setters and the UC San Diego Intermediate Software Development
+ * MOOC team
+ * @author Ronny MacMaster
+ */
+public class AirportMarker extends CommonMarker {
+	public static List<SimpleLinesMarker> routes;
+	
+	public AirportMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+	
+	}
+	
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		pg.fill(255,155,0);
+		pg.ellipse(x, y, 5, 5);
+	}
+
+	@Override
+	public void showTitle(PGraphics pg, float x, float y) {
+		String title = getTitle();
+		pg.pushStyle();
+		
+		pg.rectMode(PConstants.CORNER);
+		
+		pg.stroke(110);
+		pg.fill(255,255,255);
+		pg.rect(x, y + 15, pg.textWidth(title) +6, 18, 5);
+		
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.fill(0);
+		pg.text(title, x + 3 , y +18);
+		
+		pg.popStyle();
+		
+	}
+	
+	public String getTitle() {
+		return (String) getProperty("name")+" International Airport";	
+	}
+	
+}
