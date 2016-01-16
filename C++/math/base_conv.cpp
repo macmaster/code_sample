9dde69973438c15957afcdbe96037229cf8bdf4c
ronny macmaster <ronnymacmaster@gmail.com>
2016-01-15 19:39:34 -0600

    begin base_conv.cpp

---
+++ C++/math/base_conv.cpp
@@ -0,0 +1,4 @@
+/******************************* Base Conversion ********************************/
+/* Author: Ronald MacMaster
+ * Date: 1/15/2016
+ * Description: Simple script which converts input number to another base 		  */
