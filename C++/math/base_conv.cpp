1d07602a56c6cd3d1ef2278d4f1fdbacdf7aeda3
ronny macmaster <ronnymacmaster@gmail.com>
2016-01-15 19:45:38 -0600

    clean math dir

--- C++/math/base_conv.cpp
+++ C++/math/base_conv.cpp
@@ -1,4 +1,32 @@
 /******************************* Base Conversion ********************************/
 /* Author: Ronald MacMaster
  * Date: 1/15/2016
- * Description: Simple script which converts input number to another base 		  */
+ * Description: Simple script which converts input number to another base       */
+ 
+ #include <iostream>
+ #include <unordered_map>
+ 
+ using namespace std;
+ 
+ typedef int number;
+ int main()
+ {
+	 
+	 char flag = 'n';
+	 string input, output;
+	 unordered_map < char, int> base_map, value_map; 
+	 
+	 
+	 while((flag != 'y') || (flag != 'n'))
+	 {
+		 //welcome prompt
+		 cout << "Welcome to Base Converter" << endl;
+		 cout << "Enter your number: "; cin >> input_num;
+		 
+		 //quit prompt
+		 cout << "Would you like to quit? (y/n)";
+		 
+	 }
+	 
+ 
+ }
