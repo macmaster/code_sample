b59af66bf6f2f71f463359259c85bdc5ece10d41
ronny macmaster <ronnymacmaster@gmail.com>
2016-01-22 21:18:25 -0600

    LRU Cache C++

---
+++ C++/cache/main.cpp
@@ -0,0 +1,20 @@
+int main() {
+   int n, capacity,i;
+   cin >> n >> capacity;
+   LRUCache l(capacity);
+   for(i=0;i<n;i++) {
+      string command;
+      cin >> command;
+      if(command == "get") {
+         int key;
+         cin >> key;
+         cout << l.get(key) << endl;
+      } 
+      else if(command == "set") {
+         int key, value;
+         cin >> key >> value;
+         l.set(key,value);
+      }
+   }
+   return 0;
+}
