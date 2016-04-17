db07752d483f5d78ba9c1d6160ba8732565ccd9d
ronny macmaster <ronnymacmaster@gmail.com>
2016-04-17 04:51:32 -0500

    with sets

--- misc/hacking/unsc.py
+++ misc/hacking/unsc.py
@@ -6,25 +6,33 @@
 # Author : ronny macmaster
 # date: 4/17/2016
 
-import re
+from sets import Set
 
+# open input
 answer = ""
 soup = open("input1.txt") # word scramble file
 for word in soup:
+    
+    # build scramble set
     word = word.rstrip()
-
+    wchars = Set(word)
+    
     # compare to words in wordlist
     with open("wordlist")as wordlist:
         for line in wordlist:
+            # build line set
+            line = line.rstrip()
+            lchars = Set(line)
             wordfound = True
-            for char in word:
-                if line.find(char) == -1:
-                    wordfound = False
-                    break
-            if wordfound:
-                answer += line.rstrip() + ","
+
+            # compare word sets
+            if wchars <= lchars and wchars >= lchars:
+                answer += line + ","
+                print line
                 break
-print answer
+            
+# print csv answer
+print answer.rstrip(",")
             
                 
                     
