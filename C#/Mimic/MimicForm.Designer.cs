48ef098bd120cf35777faa4383a0f5b1a78bfb4d
ronny macmaster <ronnymacmaster@gmail.com>
2016-04-22 18:38:17 -0500

    mimic tool

---
+++ C#/Mimic/MimicForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Mimic
+{
+    partial class MimicForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MimicForm));
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IDBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.IDBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Green;
+            this.start.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(148, 118);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(113, 31);
+            this.start.TabIndex = 0;
+            this.start.Text = "start mimic";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Habbo Mimic v1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(39, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chat ID:";
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Red;
+            this.stop.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.Location = new System.Drawing.Point(148, 161);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(113, 31);
+            this.stop.TabIndex = 4;
+            this.stop.Text = "stop mimic";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Location = new System.Drawing.Point(74, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "By: Ronny Macmaster";
+            // 
+            // IDBox
+            // 
+            this.IDBox.Hexadecimal = true;
+            this.IDBox.Location = new System.Drawing.Point(59, 152);
+            this.IDBox.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(37, 20);
+            this.IDBox.TabIndex = 6;
+            // 
+            // MimicForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(301, 301);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MimicForm";
+            this.Text = "Habbo Mimic v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.IDBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown IDBox;
+    }
+}
+
---
+++ C#/Mimic/MimicForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Sulakore.Extensions;
+using Sulakore.Communication;
+using Sulakore.Protocol;
+
+namespace Mimic
+{
+    public partial class MimicForm : ExtensionForm
+    {
+        private bool mimicFlag = false;
+        private byte copyID = 0;
+
+        public MimicForm()
+        {
+            // set triggers
+            Triggers.InAttach(1232, OnChatIncoming);
+            InitializeComponent();
+        }
+
+        // flip the habbo chat string
+        protected void OnChatIncoming(InterceptedEventArgs obj)
+        {
+
+            if (mimicFlag)
+            {
+                // byteify the packet
+                HMessage hmsg = obj.Packet;
+                byte[] data = hmsg.ToBytes();
+
+                // parse msg length and sender key
+                byte sender = data[9];
+                int len = data[11];
+
+                if (sender == copyID)
+                {
+                    // parse msg string
+                    List<byte> strlist = data.ToList();
+                    strlist = strlist.GetRange(12, len + 12);
+                    string char_string = Encoding.UTF8.GetString(strlist.ToArray());
+
+                    // print msg string
+                    Connection.SendToServerAsync(3871, char_string, 4, 0);
+                }
+            }
+        }
+
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            // clear text box and begin mimic
+            if (!mimicFlag)
+            {
+                copyID = (byte)IDBox.Value;
+                mimicFlag = true;
+            }
+
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            mimicFlag = false;
+        }
+
+    }
+}
