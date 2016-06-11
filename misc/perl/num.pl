bee82da2a21f71bed55b57e4cf4b31ddb18f16ea
Ronny Macmaster <RonnyMacmaster@gmail.com>
2016-06-11 00:25:26 -0500

    hashing added to the perl script

--- misc/perl/num.pl
+++ misc/perl/num.pl
@@ -7,6 +7,7 @@
 
 # Scalars
 $x = "1"; $y = 1;
+print "\n", "Scalars:\n";
 print "x = $x y = $y \n";
 print 'x + y = ', $x + $y, "\n";
 print 'x - y = ', $x - $y, "\n";
@@ -14,9 +15,19 @@ print 'x * y = ', $x * $y, "\n";
 print 'x / y = ', $x / $y, "\n";
 
 # Arrays
+# you can go backwards, but it returns null past the boundary.
 
 @arr = (0, 1, 2, 3, 4, 5);
+print "\n\n", "Arrays:\n";
 print "$arr[0], $arr[1], $arr[2], $arr[3], $arr[4], $arr[5]\n";
 print "$arr[-1], $arr[-2], $arr[-3], $arr[-4], $arr[-5], $arr[-6]\n";
-print "$arr[6] = $arr[-7]", "\n"; # you can go backwards, but it returns null at the boundary.
+print "$arr[6] = $arr[-7]", "\n"; 
 
+# Hashes
+
+%hash = ("key1", "value", "key2", "value", "key3", 100);
+print "\n\n", "Hashes:\n";
+print "key1 value: \"$hash{'key1'}\"\n";
+print "key2 value: \"$hash{'key2'}\"\n";
+print "key3 value: $hash{'key3'}\n";
+print "value access as key: $hash{'value'}\n";
