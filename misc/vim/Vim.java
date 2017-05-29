2ef23565a3a5987acb54b8a4eec14516448d55d1
ronny <ronnymacmaster@gmail.com>
2017-05-29 12:38:01 -0400

    mystic

---
+++ misc/vim/Vim.java
@@ -0,0 +1,16 @@
+public class Vim {
+
+	public static void main (String[] args){
+		System.out.println("This is my vim test driver...");
+		
+		// some code version 1
+		//
+		// ok then bud...
+		// back to work here. nice...
+
+		// some code version 2
+		//
+
+	}
+
+}
