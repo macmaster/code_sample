7655adfabe41ced9fbfd081da2a3dfb943f59437
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-02-12 23:50:37 -0500

    a basic ruby shell (with some added security)

---
+++ ruby/shell.rb
@@ -0,0 +1,67 @@
+def start_msg(timestamp)
+  "The ruby journey begins...\n" \
+  "The current time is #{timestamp}"
+end
+
+def walk(direction, num_paces)
+  if direction == :north
+    puts "walking #{direction} #{num_paces} paces."
+  elsif direction == :east
+    puts "walking #{direction} #{num_paces} paces."
+  elsif direction == :south
+    puts "walking #{direction} #{num_paces} paces."
+  elsif direction == :west
+    puts "walking #{direction} #{num_paces} paces."
+  else
+    puts "bad walking direction."
+  end
+end
+
+msg = start_msg(Time.now)
+puts msg
+
+array = [ 1, "nice", 3.14, nil]
+puts "The array declared on line 9 is: #{array.inspect}"
+
+puts "invocation of the %W built-in shortcut"
+array = %W(
+  sometimes
+  life
+  can be
+  a little
+  irritating
+  #{array.inspect}
+)
+puts array.inspect
+
+histogram = Hash.new(-1)
+histogram[:north] = "north"
+histogram[:east] = "east"
+histogram[:south] = "south"
+histogram[:west] = "west"
+puts "histogram[:up] = #{histogram[:up]}"
+
+puts "\n" # banner
+puts "beginning backpack trip:"
+walk(:north, 5)
+walk(:south, 4)
+walk(:west, 0)
+walk(:east, -6)
+walk(:up, 6)
+
+i = 6
+while i > 0 and true
+  puts "#{i -= 1}) times. lemme see you do it 2 times."
+end
+
+print "$ "
+while line = gets
+  # puts line.upcase
+  if line =~ /(perl|python)/
+    puts "nice try buddy! that's a banned command."
+  else
+    system( line )
+  end
+  print "$ "
+end
+puts ""
--- ruby/time.rb
+++
@@ -1,8 +0,0 @@
-def start_msg(timestamp)
-  "The ruby journey begins...\n" \
-  "The current time is #{timestamp}"
-end
-
-msg = start_msg(Time.now)
-puts msg
-
