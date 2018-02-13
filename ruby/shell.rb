ed9c7a70ed9693825bcb65ecfb5c59a08a1c3ec6
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-02-13 00:07:26 -0500

    more shell updates

--- ruby/shell.rb
+++ ruby/shell.rb
@@ -17,51 +17,67 @@ def walk(direction, num_paces)
   end
 end
 
+def call_block
+  puts "start call_block"
+  yield
+  yield
+  puts "middle call_block"
+  yield
+  puts "end call_block"
+end
+
 msg = start_msg(Time.now)
 puts msg
 
-array = [ 1, "nice", 3.14, nil]
-puts "The array declared on line 9 is: #{array.inspect}"
+# array = [ 1, "nice", 3.14, nil]
+# puts "The array declared on line 9 is: #{array.inspect}"
+# 
+# puts "invocation of the %W built-in shortcut"
+# array = %W(
+#   sometimes
+#   life
+#   can be
+#   a little
+#   irritating
+#   #{array.inspect}
+# )
+# puts array.inspect
+# 
+# histogram = Hash.new(-1)
+# histogram[:north] = "north"
+# histogram[:east] = "east"
+# histogram[:south] = "south"
+# histogram[:west] = "west"
+# puts "histogram[:up] = #{histogram[:up]}"
+# 
+# puts "\n" # banner
+# puts "beginning backpack trip:"
+# walk(:north, 5)
+# walk(:south, 4)
+# walk(:west, 0)
+# walk(:east, -6)
+# walk(:up, 6)
+# 
+# i = 6
+# while i > 0 and true
+#   puts "#{i -= 1}) times. lemme see you do it 2 times."
+# end
 
-puts "invocation of the %W built-in shortcut"
-array = %W(
-  sometimes
-  life
-  can be
-  a little
-  irritating
-  #{array.inspect}
-)
-puts array.inspect
+# # shell
+# print "$ "
+# while line = gets
+#   # puts line.upcase
+#   if line =~ /(perl|python)/
+#     puts "nice try buddy! that's a banned command."
+#   else
+#     system( line )
+#   end
+#   print "$ "
+# end
+# puts ""
 
-histogram = Hash.new(-1)
-histogram[:north] = "north"
-histogram[:east] = "east"
-histogram[:south] = "south"
-histogram[:west] = "west"
-puts "histogram[:up] = #{histogram[:up]}"
+string = "This is a really long string with a few misnomers and what not brother...."
+puts "original: #{string}"
+puts "substituted: #{string.gsub(/[aeiou]/, 'x')}"
 
-puts "\n" # banner
-puts "beginning backpack trip:"
-walk(:north, 5)
-walk(:south, 4)
-walk(:west, 0)
-walk(:east, -6)
-walk(:up, 6)
-
-i = 6
-while i > 0 and true
-  puts "#{i -= 1}) times. lemme see you do it 2 times."
-end
-
-print "$ "
-while line = gets
-  # puts line.upcase
-  if line =~ /(perl|python)/
-    puts "nice try buddy! that's a banned command."
-  else
-    system( line )
-  end
-  print "$ "
-end
-puts ""
+call_block { puts "wow.wow.wow!" }
